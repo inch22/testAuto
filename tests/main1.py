413d0bfb67d27136aeffe245e3559daf3ee46144
inch22 <inch22@mail.ru>
2023-01-17 14:17:49 +0300

    first

--- tests/main1.py
+++ tests/main1.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("http://selenium.dev/selenium/web/web-form.html")
-input_username = driver.find_element(By.XPATH,'//*[@id="my-text-id"]')
+input_username = driver.find_element(By.XPATH,'/html/body/main/div/form/div/div[2]/div[3]')
 if input_username is None:
     print("Элемент не найден")
 else:
--- tests/main2.py
+++ tests/main2.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("http://selenium.dev/selenium/web/web-form.html")
-input_username = driver.find_element(By.CSS_SELECTOR,'body > main > div > form > div > div:nth-child(2) > div:nth-child(4) > label:nth-child(1)')
+input_username = driver.find_element(By.CSS_SELECTOR,'body > main > div > form > div > div:nth-child(2) > div:nth-child(4) > label:nth-child(2)')
 if input_username is None:
     print("Элемент не найден")
 else:

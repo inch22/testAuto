0e761ff3b9124cfce6815639ec146c25b9106c39
inch22 <inch22@mail.ru>
2023-01-17 14:11:34 +0300

    first

--- tests/main2.py
+++ tests/main2.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-driver.get("http://selenium.dev/selenium/web/web-form.html/")
-input_username = driver.find_element(By.CSS_SELECTOR,'body > main > div > form > div > div:nth-child(4) > input')
+driver.get("http://selenium.dev/selenium/web/web-form.html")
+input_username = driver.find_element(By.CSS_SELECTOR,'body > main > div > form > div > div:nth-child(2) > div:nth-child(4) > label:nth-child(1)')
 if input_username is None:
     print("Элемент не найден")
 else:

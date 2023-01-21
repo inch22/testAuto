5fe56d640e4e111e75abcaa5c86233cd49267ba0
inch22 <inch22@mail.ru>
2023-01-21 11:28:19 +0300

    first

--- DZ/check.py
+++ DZ/check.py
@@ -11,6 +11,7 @@ class Check:
 
 home =  Check("https://guruvkusa.ru/")
 home.exist()
+#driver = webdriver.Chrome()
 input_username = driver.find_element(By.CSS_SELECTOR,'body > div.page_layout.page_layout-clear > header > div.layout.widget-type_widget_v4_header_4_6b677bbb4943ee7655d464f5e60d70e2 > div > div.header.header_no-languages > div.header-mobile-panel > div > div.header-mobile-panel__bottom > button')
 if input_username is None:
     print("Элемент не найден")
